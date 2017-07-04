8d3ba111ff39b6e22527ef4cd181878684fdd230
Peter7777777 <371796262@qq.com>
2017-07-04 17:58:28 +0800

    机器学习代码

---
+++ Example/8.4.overfit.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+import matplotlib as mpl
+
+
+def xss(y, y_hat):
+    y = y.ravel()
+    y_hat = y_hat.ravel()
+    # Version 1
+    tss = ((y - np.average(y) ** 2).sum())
+    rss = ((y_hat - y) ** 2).sum()
+    r2 = 1 - rss / tss
+    # Version 2
+    tss = np.var(y)
+    rss = np.average((y_hat - y) ** 2)
+    r2 = 1 - rss / tss
+    corr_coef = np.corrcoef(y, y_hat)[0, 1]
+    return r2, corr_coef
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    N = 9
+    x = np.linspace(0, 6, N) + np.random.randn(N)
+    x = np.sort(x)
+    y = x ** 2 - 4 * x - 3 + np.random.randn(N)
+    x.shape = -1, 1
+    y.shape = -1, 1
+
+    model_1 = Pipeline([
+        ('poly', PolynomialFeatures()),
+        ('linear', LinearRegression(fit_intercept=False))])
+    model_2 = Pipeline([
+        ('poly', PolynomialFeatures()),
+        ('linear', RidgeCV(alphas=np.logspace(-3, 2, 100), fit_intercept=False))])
+    model_3 = Pipeline([
+        ('poly', PolynomialFeatures()),
+        ('linear', LassoCV(alphas=np.logspace(-3, 2, 100), fit_intercept=False))])
+    models = model_1, model_2, model_3
+    mpl.rcParams['font.sans-serif'] = [u'simHei']
+    mpl.rcParams['axes.unicode_minus'] = False
+    np.set_printoptions(suppress=True)
+
+    plt.figure(figsize=(8, 11), facecolor='w')
+    d_pool = np.arange(1, N, 1)  # 阶
+    m = d_pool.size
+    clrs = []  # 颜色
+    for c in np.linspace(16711680, 255, m):
+        clrs.append('#%06x' % c)
+    line_width = np.linspace(5, 2, m)
+    titles = u'线性回归', u'Ridge回归', u'LASSO'
+    for t in range(3):
+        model = models[t]
+        plt.subplot(3, 1, t + 1)
+        plt.plot(x, y, 'ro', ms=10, zorder=N)
+        for i, d in enumerate(d_pool):
+            model.set_params(poly__degree=d)
+            model.fit(x, y)
+            lin = model.get_params('linear')['linear']
+            if t == 0:
+                print u'线性回归: %d阶，系数为：' % d, lin.coef_.ravel()
+            else:
+                print u'岭回归/LASSO: %d阶，alpha=%.6f，系数为：' % (d, lin.alpha_), lin.coef_.ravel()
+            x_hat = np.linspace(x.min(), x.max(), num=100)
+            x_hat.shape = -1, 1
+            y_hat = model.predict(x_hat)
+            s = model.score(x, y)
+            print s, '\n'
+            zorder = N - 1 if (d == 2) else 0
+            plt.plot(x_hat, y_hat, color=clrs[i], lw=line_width[i], label=(u'%d阶，score=%.3f' % (d, s)), zorder=zorder)
+        plt.legend(loc='upper left')
+        plt.grid(True)
+        plt.title(titles[t], fontsize=16)
+        plt.xlabel('X', fontsize=14)
+        plt.ylabel('Y', fontsize=14)
+    plt.tight_layout(1, rect=(0, 0, 1, 0.95))
+    plt.suptitle(u'多项式曲线拟合', fontsize=18)
+    plt.show()
