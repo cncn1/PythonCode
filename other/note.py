351352e99cfc78bca884824a55fb834ade2794b7
Peter7777777 <Peter7777777@users.noreply.github.com>
2017-09-15 21:57:07 +0800

    修改测试

--- other/note.py
+++ other/note.py
@@ -29,13 +29,18 @@ A = np.array([[1, -2, 1], [0, 2, -1], [1, 1, -2]])
 A_abs = np.linalg.det(A)  # 求A的行列式的值
 print A_abs
 
-B = np.linalg.inv(A)    # 求A的逆
+B = np.linalg.inv(A)  # 求A的逆
 print B
 
-A_bansui = B * A_abs    # 求A的伴随
+A_bansui = B * A_abs  # 求A的伴随
 print A_bansui
 # np.3 计算阶乘
 print np.math.factorial(125)
+# np.4 tile 函数
+
+# tile(a,x):   x是控制a重复几次的，结果是一个一维数组
+# tile(a,(x,y))：   结果是一个二维矩阵，其中行数为x，列数是一维数组a的长度和y的乘积
+# tile(a,(x,y,z)):   结果是一个三维矩阵，其中矩阵的行数为x，矩阵的列数为y，而z表示矩阵每个单元格里a重复的次数。
 
 # pd.1 读取cvs文件
 import pandas
@@ -56,3 +61,21 @@ from itertools import combinations, permutations
 s1 = list(permutations([1, 2, 3], 2))
 s2 = list(combinations([1, 2, 3], 2))
 print s1, '\n', s2
+
+# o.3.1 copy.copy 浅拷贝 只拷贝父对象，不会拷贝对象的内部的子对象。
+# o.3.2 copy.deepcopy 深拷贝 拷贝对象及其子对象
+import copy
+
+a = [1, 2, 3, 4, ['a', 'b']]  # 原始对象
+
+b = a  # 赋值，传对象的引用
+c = copy.copy(a)  # 对象拷贝，浅拷贝
+d = copy.deepcopy(a)  # 对象拷贝，深拷贝
+
+a.append(5)  # 修改对象a
+a[4].append('c')  # 修改对象a中的['a', 'b']数组对象
+
+print 'a = ', a
+print 'b = ', b
+print 'c = ', c
+print 'd = ', d
